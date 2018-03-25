2575f517f8b2eba720549ba6dba660e61a789e4c
A Q <you@example.com>
2018-03-24 21:04:26 -0400

    added different way to get animals

--- src/main/java/com/aandmcoding/springdemo/MainController.java
+++ src/main/java/com/aandmcoding/springdemo/MainController.java
@@ -3,6 +3,7 @@ package com.aandmcoding.springdemo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,5 +24,36 @@ public class MainController {
     public String getWords(Model model){
         model.addAttribute("word", animal.getName());
         return "index";
+    }
+
+
+    /*
+    code added by alfred
+
+    this should allow you to enter a different name in the url
+    and a different aniaml and description each time
+
+     */
+    private Animal[] animals = {new Animal("Ant", "Has four legs"),
+            new Animal("dog", "Like to wag its tail"),
+            new Animal("cat", "like to chase after mice"),
+            new Animal("bird", "like to to fly"),
+            new Animal("cow", "like to eat grass")};
+
+    @RequestMapping("/{name}")
+    public String getAnimal(@PathVariable String name, Model model){
+
+        String animal = null;
+        String desc = null;
+        for(Animal a: animals){
+            if (a.getName().equalsIgnoreCase(name)){
+                animal = a.getName();
+                desc = a.getDescription();
+                model.addAttribute("name", name);
+                model.addAttribute("desc", desc);
+                return "index";
+            }
+        }
+        return null;
     }
 }

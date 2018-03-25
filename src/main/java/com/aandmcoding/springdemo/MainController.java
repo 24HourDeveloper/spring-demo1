87c122e83d58e2e4a237d244f19710a187550ab7
24HourDeveloper <grownmanherbal@gmail.com>
2018-03-25 10:17:52 -0400

    added AnimalRepository but object is showing hashcode.

--- src/main/java/com/aandmcoding/springdemo/MainController.java
+++ src/main/java/com/aandmcoding/springdemo/MainController.java
@@ -1,27 +1,30 @@
 package com.aandmcoding.springdemo;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-    private Animal animal = new Animal("Ant", "Has for legs");
-    private Animal animal2 = new Animal("Dog", "Has black long hair and great sense of smell");
+//    private Animal animal = new Animal("Ant", "Has for legs");
+//    private Animal animal2 = new Animal("Dog", "Has black long hair and great sense of smell");
+    @Autowired
+    private  AnimalRepository animalRepository;
 
     @RequestMapping("/")
     public String getWord(Model model){
-        model.addAttribute("word", animal.getName());
-        model.addAttribute("wordd", animal.getDescription());
-        model.addAttribute("animalName2", animal2.getName());
-        model.addAttribute("animalDescription2", animal2.getDescription());
+        model.addAttribute("word", animalRepository.allAnimals());
+        //model.addAttribute("wordd", animalRepositor);
+        //model.addAttribute("animalName2", animal2.getName());
+        //model.addAttribute("animalDescription2", animal2.getDescription());
         return "index";
     }
 
     @RequestMapping("/animal")
     public String getWords(Model model){
-        model.addAttribute("word", animal.getName());
+        //model.addAttribute("word", animal.getName());
         return "index";
     }
 }

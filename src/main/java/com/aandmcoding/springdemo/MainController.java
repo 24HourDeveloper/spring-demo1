87b00355457904406ccd4831b030eca5b02d3404
A Q <you@example.com>
2018-03-25 22:50:07 -0400

    new page for alfreds part

--- src/main/java/com/aandmcoding/springdemo/MainController.java
+++ src/main/java/com/aandmcoding/springdemo/MainController.java
@@ -51,7 +51,7 @@ public class MainController {
                 desc = a.getDescription();
                 model.addAttribute("name", name);
                 model.addAttribute("desc", desc);
-                return "index";
+                return "animals";
             }
         }
         return null;
